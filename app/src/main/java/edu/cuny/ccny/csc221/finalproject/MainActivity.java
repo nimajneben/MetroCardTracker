4e6cb1e98515cd08ee2a6181e273256d293cbca8
Benjamin <47288387+nimajneben@users.noreply.github.com>
2021-04-26 12:27:25 -0400

    initial commit

---
+++ app/src/main/java/edu/cuny/ccny/csc221/finalproject/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.cuny.ccny.csc221.finalproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
