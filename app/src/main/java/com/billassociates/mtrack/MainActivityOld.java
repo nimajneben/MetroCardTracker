afa72da6dfe846f9f08cd0a820bb64346522403b
Benjamin <47288387+nimajneben@users.noreply.github.com>
2021-05-11 01:48:12 -0400

    migrate package name

--- app/src/main/java/com/billassociates/mtrack/MainActivityOld.java
+++ app/src/main/java/com/billassociates/mtrack/MainActivityOld.java
@@ -5,11 +5,11 @@ import android.os.Bundle;
 
 import edu.cuny.ccny.csc221.finalproject.R;
 
-public class MainActivity extends Activity {
+public class MainActivityOld extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_old);
     }
 }
