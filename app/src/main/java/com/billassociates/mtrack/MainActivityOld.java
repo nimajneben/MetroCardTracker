92f2a61a9c9550b588c68b6562f0bf81b5878434
Benjamin <47288387+nimajneben@users.noreply.github.com>
2021-05-15 01:38:58 -0400

    add button actions on home screen

--- app/src/main/java/com/billassociates/mtrack/MainActivityOld.java
+++ app/src/main/java/com/billassociates/mtrack/MainActivityOld.java
@@ -1,7 +1,11 @@
 package com.billassociates.mtrack;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import edu.cuny.ccny.csc221.finalproject.R;
 
@@ -11,5 +15,23 @@ public class MainActivityOld extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_old);
+    }
+
+
+    public void addMetroCardBtnClick(View v)
+    {
+        Context c = getApplicationContext();
+        CharSequence text = "Hi I pressed the button.";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast t = Toast.makeText(c,text,duration);
+
+        t.show();
+        goToEditActivity(v);
+    }
+
+    public void goToEditActivity(View view) {
+        Intent intent = new Intent(this, MetrocardEditActivity.class);
+        startActivity(intent);
     }
 }
