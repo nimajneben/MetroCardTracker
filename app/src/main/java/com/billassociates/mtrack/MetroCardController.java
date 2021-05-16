d89fe6c5fe597f7b9906e100c458b3c11ef4760a
Benjamin <47288387+nimajneben@users.noreply.github.com>
2021-05-16 14:04:03 -0400

    add discard change btn

---
+++ app/src/main/java/com/billassociates/mtrack/MetroCardController.java
@@ -0,0 +1,60 @@
+package com.billassociates.mtrack;
+
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class MetroCardController {
+    static private ArrayList<MetrocardData> arrayList = new ArrayList<>();
+    static private MetrocardData current_mcard_ptr;
+
+    //public statistics
+    public static final int FARE = 275;
+
+
+    public ArrayList<MetrocardData> getArrayList() {
+        return arrayList;
+    }
+
+    public void ReadToArrayList(Context c)
+    {
+        try
+        {
+            FileInputStream fis = c.openFileInput("metrocard.bin");
+            ObjectInputStream os = new ObjectInputStream(fis);
+            arrayList = (ArrayList<MetrocardData>) os.readObject();
+
+        }
+        catch (Exception fnfe)
+        {
+            String errormsg = "Cannot read in object!!";
+            System.err.println(errormsg);
+            Toast t = Toast.makeText(c,errormsg,Toast.LENGTH_SHORT);
+            t.show();
+
+        }
+
+
+    }
+
+    public void SaveFromArrayList(Context c) throws IOException
+    {
+        FileOutputStream fos = c.openFileOutput("metrocard.bin", Context.MODE_PRIVATE);
+        ObjectOutputStream os = new ObjectOutputStream(fos);
+        os.writeObject(arrayList);
+        os.close();
+    }
+
+
+
+}
---
+++ app/src/main/java/com/billassociates/mtrack/MetrocardCalculateActivity.java
@@ -0,0 +1,15 @@
+package com.billassociates.mtrack;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import edu.cuny.ccny.csc221.finalproject.R;
+
+public class MetrocardCalculateActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_metrocard_calculate);
+    }
+}
