d6bb47262176613b18379100aa60d439df1689d8
Benjamin <47288387+nimajneben@users.noreply.github.com>
2021-05-16 14:06:39 -0400

    add array datastruct

--- app/src/main/java/com/billassociates/mtrack/MainActivityOld.java
+++ app/src/main/java/com/billassociates/mtrack/MainActivityOld.java
@@ -5,7 +5,12 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
 
 import edu.cuny.ccny.csc221.finalproject.R;
 
@@ -21,17 +26,44 @@ public class MainActivityOld extends Activity {
     public void addMetroCardBtnClick(View v)
     {
         Context c = getApplicationContext();
+
+
+
+        // Toast Test
         CharSequence text = "Hi I pressed the button.";
         int duration = Toast.LENGTH_SHORT;
 
         Toast t = Toast.makeText(c,text,duration);
 
         t.show();
+
+
+        // Populate ListView Test
+        ListView lv = (ListView) findViewById(R.id.McardList);
+        MetroCardController mc = new MetroCardController();
+        mc.ReadToArrayList(c);
+        ArrayList<MetrocardData> arrayList = mc.getArrayList();
+        arrayList.add(new MetrocardData("Test",232));
+        ArrayAdapter<MetrocardData> arrayAdapter = new ArrayAdapter<MetrocardData>
+                (this, android.R.layout.simple_list_item_1,arrayList);
+        lv.setAdapter(arrayAdapter);
+
+
         goToEditActivity(v);
+    }
+
+    public void populateList()
+    {
+
     }
 
     public void goToEditActivity(View view) {
         Intent intent = new Intent(this, MetrocardEditActivity.class);
+        startActivity(intent);
+    }
+
+    public void goToViewActivity(View view) {
+        Intent intent = new Intent(this, MetrocardCalculateActivity.class);
         startActivity(intent);
     }
 }
